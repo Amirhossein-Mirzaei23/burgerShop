629e096cd3286c0382902e8b3c8a110d03bc7436
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-02-17 21:47:43 +0330

    explain the components of app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
+    <!--header layout--->
     <LayoutHeader />
+    <!--use default nuxt loading page component--->
     <NuxtLoadingIndicator />
     <NuxtPage />
+    <!--footer lay out--->
     <LayoutFooter />
   </div>
 </template>
