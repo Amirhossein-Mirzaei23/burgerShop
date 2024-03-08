c3b8d844799fa2472761cfe0701b4bd54d0e4ac4
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-03-08 23:00:44 +0330

    Update payment.vue

--- components/cart/payment.vue
+++ components/cart/payment.vue
@@ -9,18 +9,20 @@
 
 const props = defineProps(['coupon', 'addressId', 'cart'])
 
-
+/// store loader position in variable
 const loading = ref(false);
-
+/// check payment postion with database
 async function payment() {
+/// check address 
     if (props.addressId === '' || props.addressId === null) {
         toastr.error('انتخاب آدرس الزامی است')
         return;
     }
-
+/// check payment postion with database
     try {
+ ///enable loader
         loading.value = true;
-
+// send cart ,coupon and address data for payment
         const data = await $fetch('/api/payment/send', {
             method: 'POST',
             body: {
@@ -29,14 +31,16 @@ async function payment() {
                 address_id: props.addressId
             }
         })
-
+//
         await navigateTo(data.url, {
             external: true
         })
 
     } catch (error) {
+// modrate errors
         toastr.error(Object.values(error.data.data.message).flat().toString())
     } finally {
+ ///disable loader
         loading.value = false;
     }
 }
